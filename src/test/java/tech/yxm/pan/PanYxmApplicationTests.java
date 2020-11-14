2d958d2e3190076c8d006726ba35135937efc355
River-HE <1273019479@qq.com>
2020-11-14 15:37:40 +0800

    init

---
+++ src/test/java/tech/yxm/pan/PanYxmApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.yxm.pan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PanYxmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
